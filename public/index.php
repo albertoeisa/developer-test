3684dba00a51ba54c98b2ec56f08b74ac1e08b0f
Ben Grout <ben@bgrout.co.uk>
2016-08-01 13:42:16 +0100

    change sorting signature

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ $res = $client->request('GET', 'http://3ev.org/dev-test-api/');
 $data = json_decode($res->getBody(), true);
 
 //Sort the episodes
-array_multisort(array_keys($data), SORT_ASC, $data);
+array_multisort(array_keys($data), SORT_ASC, SORT_STRING, $data);
 
 //Render the template
 echo $twig->render('page.html', ["episodes" => $data]);
