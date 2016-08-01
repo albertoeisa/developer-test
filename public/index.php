2e6059c220f4994a8a683647dc7e3c6af5f11f61
Ben <ben@3ev.com>
2016-08-01 10:55:49 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,17 @@
 <?php
 require_once '../vendor/autoload.php';
 
+//Load Twig templating environment
 $loader = new Twig_Loader_Filesystem('../templates/');
 $twig = new Twig_Environment($loader, ['debug' => true]);
-$twig->addExtension(new Twig_Extension_Debug());
 
-
+//Get the episodes from the API
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'http://3ev.org/dev-test-api/');
 $data = json_decode($res->getBody(), true);
 
+//Sort the episodes
 array_multisort(array_keys($data), SORT_ASC, $data);
 
+//Render the template
 echo $twig->render('page.html', ["episodes" => $data]);
